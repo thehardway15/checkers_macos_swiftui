8d6ad51da05d0d4b19f57026c85e973c5ded6f75
Damian Wiśniewski <dev@thehardway.pl>
2023-01-15 10:41:20 +0100

    Initial Commit

---
+++ checkers/checkersApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkersApp.swift
+//  checkers
+//
+//  Created by Damian Wiśniewski on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct checkersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
